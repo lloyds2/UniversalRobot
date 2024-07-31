e7a425c1f6ed9c706962ae429ae1296abc107ccb
lloyds2 <sophie.a.lloyd@medtronic.com>
2024-07-31 14:27:19 -0600

    Trial using URBasic

---
+++ UR_programs/connect_trial.py
@@ -0,0 +1,108 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul 31 12:23:36 2024
+
+@author: lloyds2
+
+
+"""
+
+import URBasic
+import math
+import numpy as np
+import sys
+import cv2
+import time
+import math3d as m3d
+
+"""SETTINGS AND VARIABLES ________________________________________________________________"""
+
+
+ROBOT_IP = '10.0.2.15'
+ACCELERATION = 0.4  # Robot acceleration value
+VELOCITY = 0.4  # Robot speed value
+
+# The Joint position the robot starts at
+robot_startposition = (math.radians(-218),
+                    math.radians(-63),
+                    math.radians(-93),
+                    math.radians(-20),
+                    math.radians(88),
+                    math.radians(0))
+
+
+
+# Variable which scales the robot movement from pixels to meters.
+m_per_pixel = 00.00009  
+
+# Size of the robot view-window
+# The robot will at most move this distance in each direction
+max_x = 0.2
+max_y = 0.2
+
+# Maximum Rotation of the robot at the edge of the view window
+hor_rot_max = math.radians(50)
+vert_rot_max = math.radians(25)
+
+time.sleep(0.2)
+
+
+
+"""FUNCTIONS _____________________________________________________________________________"""
+
+
+def check_max_xy(xy_coord):
+    """
+    Checks if the face is outside of the predefined maximum values on the lookaraound plane
+
+    Inputs:
+        xy_coord: list of 2 values: x and y value of the face in the lookaround plane.
+            These values will be evaluated against max_x and max_y
+
+    Return Value:
+        x_y: new x and y values
+            if the values were within the maximum values (max_x and max_y) these are the same as the input.
+            if one or both of the input values were over the maximum, the maximum will be returned instead
+    """
+
+    x_y = [0, 0]
+    #print("xy before conversion: ", xy_coord)
+
+    if -max_x <= xy_coord[0] <= max_x:
+        # checks if the resulting position would be outside of max_x
+        x_y[0] = xy_coord[0]
+    elif -max_x > xy_coord[0]:
+        x_y[0] = -max_x
+    elif max_x < xy_coord[0]:
+        x_y[0] = max_x
+    else:
+        raise Exception(" x is wrong somehow:", xy_coord[0], -max_x, max_x)
+
+    if -max_y <= xy_coord[1] <= max_y:
+        # checks if the resulting position would be outside of max_y
+        x_y[1] = xy_coord[1]
+    elif -max_y > xy_coord[1]:
+        x_y[1] = -max_y
+    elif max_y < xy_coord[1]:
+        x_y[1] = max_y
+    else:
+        raise Exception(" y is wrong somehow", xy_coord[1], max_y)
+    #print("xy after conversion: ", x_y)
+
+    return x_y
+
+def set_lookorigin():
+    """
+    Creates a new coordinate system at the current robot tcp position.
+    This coordinate system is the basis of the face following.
+    It describes the midpoint of the plane in which the robot follows faces.
+
+    Return Value:
+        orig: math3D Transform Object
+            characterises location and rotation of the new coordinate system in reference to the base coordinate system
+
+    """
+    position = robot.get_actual_tcp_pose()
+    orig = m3d.Transform(position)
+    return orig
+
